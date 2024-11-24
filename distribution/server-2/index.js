26b3448eaf480dcdc96e56690a6abc9c63e6e065
adofm <karthik.maniam008@gmail.com>
2024-11-24 22:27:14 +0530

    the distribution logic has been added it has 5 servers

---
+++ distribution/server-2/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/share", (req, res) => {
+  console.log(req.headers);
+  const share = process.env.SHARE;
+  if (!share) {
+    return res.status(404).json({ message: "Share not found." });
+  }
+  res.status(200).json({ share });
+});
+app.listen(7070);
